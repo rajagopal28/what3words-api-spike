5710432294b226128fc1749be0db3e86cb0a5bd1
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-10-26 12:45:47 +0530

    [doc] adding javadoc for main class and the wrapper class

--- src/main/java/com/w3w/rm/geo/app/Application.java
+++ src/main/java/com/w3w/rm/geo/app/Application.java
@@ -2,8 +2,17 @@ package com.w3w.rm.geo.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main What3WordsV3 which makes use of the SpringApplication to run the rest server in the mentioned jetty/tomcat embedded servers.
+ * @author Rajagopal
+ */
 @SpringBootApplication
 public class Application {
+    /**
+     * Method main() is the entry point for the application created to work with What3Words API.
+     *
+     * @param args with which the application is invoked.
+     */
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/w3w/rm/geo/app/config/What3WordsWrapper.java
+++ src/main/java/com/w3w/rm/geo/app/config/What3WordsWrapper.java
@@ -3,14 +3,27 @@ package com.w3w.rm.geo.app.config;
 import com.what3words.javawrapper.What3WordsV3;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * What3WordsWrapper helps split the What3WordsV3 instance creation from the application lifecycle. <br/>
+ * This enables decoupling of the component creation, encourages single responsibility, robust testing and unwraps the benefits of DependencyInversion. <br/>
+ * @author Rajagopal
+ */
 @Component()
 public class What3WordsWrapper {
+    // Eager initialisation of the singleton instance of What3WordsV3 API
     private static What3WordsV3 API_INSTANCE = new What3WordsV3("92RKJSSS");
+
+    /**
+     * Private constructor to block instantiation of the Wrapper class.
+     */
     private What3WordsWrapper() {
         // singleton blocking
     }
 
+    /**
+     * Method getInstance is static and gives access to the underlying What3WordsV3 API instance created and managed under this wrapper.
+     * @return What3WordsV3 instant that is created
+     */
     public What3WordsV3 getInstance() {
         return API_INSTANCE;
     }
